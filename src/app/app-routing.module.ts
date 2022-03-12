aa3bfa6efb5b1b9caf90a54c4069795bfc36994a
anildevaki1 <anildevaki1@gmail.com>
2022-03-12 10:33:39 +0530

    MIS12032022

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {  RouterModule, Routes } from '@angular/router';
 import { AuthGuardservice } from './assets/services/services';
 import { ContainerComponent } from './container/container.component';
 
